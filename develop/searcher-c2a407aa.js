ffe9282e2b1a61809445d7092f3c145012a8652e
shcherbak <shcherbak@users.noreply.github.com>
2026-08-08 17:55:58 +0000

    deploy: b62f83a7360b3b7c8e47d641f1a0430f548f6c44

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b7f432b0.js',
+                path_to_root + 'searchindex-de023fe6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
